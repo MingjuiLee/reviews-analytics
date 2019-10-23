f2840c53ba3b4b671678c6369f1388818a6ed0c1
李明叡 <leeray1208@limingruide-MacBook-Pro.local>
2019-10-22 23:49:22 -0700

    calc avg length of reviews

--- read.py
+++ read.py
@@ -1,15 +1,17 @@
 # 讀取留言檔
 data = []
 count = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:	# because read one line each time
 		data.append(line)
 		count += 1	# 快寫法
 		if count % 1000 == 0:
 			print(len(data))	# print current progess
-print(len(data))
+print('File reading is completed, total %d data' %(len(data)))
 
-#print(data)  # too much
-print(data[0])
-print('---------------------')
-print(data[1])
+sum_len = 0
+for d in data:	# d is a string
+	sum_len += len(d)	# count len for all d
+	#print(sum_len)
+print('Average length for reviews is %f' %(sum_len / len(data)))
